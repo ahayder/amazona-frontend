13a45d9e277212bf997d7393f13f2ebb358dcc0c
N. M. Ali Hayder <ahayder@loobylab.com>
2020-11-06 04:05:39 +0600

    redux first stage completed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
-import React from 'react';
-import Product from './components/Product';
-import data from './data';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div className="grid-container">
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              amazona
+            </a>
+          </div>
+          <div className="">
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
 
-      <header className="row">
-        <div>
-          <a className="brand" href="/">amazona</a>
-        </div>
-        <div className="">
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen} exact></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
 
-      <main>
-        <div className="row center">
-          {
-            data.products.map(product => (
-              <Product key={product._id} product={product} />
-            ))
-          }
-        </div>
-      </main>
-
-      <footer className="row center">
-        All right reserved.
-      </footer>
-
-    </div>
+        <footer className="row center">All right reserved.</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
