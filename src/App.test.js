5bee8af56f38d9e9a6656d2f8d7ae63e2624b636
ahayderbs <ali.hayder@brainstation23.com>
2020-11-21 02:05:30 +0600

    more tests added

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,20 @@
-describe("Just a fake test", () => {
-  it("should perform a fake test", () => {
+describe("This is perform some fake tests", () => {
+  it("should perform a fake truthy test", () => {
     expect(true).toBeTruthy();
+  })
+  it("should perform a fake falsy test", () => {
+    expect(false).toBeFalsy();
+  })
+  it("should be a number 5", () => {
+    expect(5).toEqual(5);
+  })
+})
+
+describe("Againi this will perform some fake tests", () => {
+  it("should be null", () => {
+    expect(null).toBeNull();
+  })
+  it("should perform a fake not truthy test", () => {
+    expect(false).not.toBeTruthy();
   })
 })
